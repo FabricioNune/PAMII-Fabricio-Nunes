37a58b9f39bf1e7e92709e327139723ceaebc092
FabricioNune <aemnernandes@gmail.com>
2023-03-08 08:36:32 -0300

    Aula sobre javascript com variveis e funcoes

---
+++ Aula02/funcoesevariaveis.js
@@ -0,0 +1,17 @@
+var variavel1 = "olá"
+let variavel2 = 3
+const variavel3 = 8
+
+console.log(variavel1, "Turminha do ", variavel2, "ano")
+console.log(variavel2 + variavel3)
+console.log(typeof variavel1)
+console.log(typeof variavel2)
+
+const obj = {
+    nome: "Fabrício",
+    signo: "Libra"
+
+}
+// Formato JSON - JAvascript Notation Object
+console.log(obj.nome, obj.signo)
+console.log(typeof obj)
