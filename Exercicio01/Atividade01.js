41c77c112acda2604c13567c418f799ca0c6327e
FabricioNune <aemnernandes@gmail.com>
2023-03-15 01:43:17 -0300

    Atividade com os operadores matematicos

---
+++ Exercicio01/Atividade01.js
@@ -0,0 +1,23 @@
+var variavel1 = 16
+var variavel2 = 4
+
+console.log("Será exibido o resultado das contas com os valores de:",variavel1,"e",variavel2)
+
+// Soma
+console.log("A soma é igual a:",variavel1 + variavel2)
+
+// Subtração
+console.log("A subtração é igual a:",variavel1 - variavel2)
+
+// Multiplicação
+console.log("A multiplicação é igual a:",variavel1 * variavel2)
+
+// Divisão
+console.log("A divisão é igual a:",variavel1 / variavel2)
+
+// Potência
+console.log ("A potência é igual a:",Math.pow(variavel1,variavel2));
+
+// Raiz quadrada
+console.log ("A raiz quadrada da 1º variável é igual a:",Math.sqrt(variavel1));
+console.log ("A raiz quadrada da 2º variável é igual a:",Math.sqrt(variavel2)); 
